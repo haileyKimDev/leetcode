ab7c33c8eceb27b41c632267f04364ae0eb4b68f
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2022-10-30 17:11:39 +0900

    Time: 1 ms (100.00%), Space: 51.4 MB (97.02%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,15 +1,21 @@
 class Solution {
     public int maxSubArray(int[] A) {
-            int n = A.length;
-            int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
-            dp[0] = A[0];
-            int max = dp[0];
+        int max_so_far = A[0];
+        int max_ending_here = 0;
+        int start = 0,end = 0, s=0;
 
-            for(int i = 1; i < n; i++){
-                dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
-                max = Math.max(max, dp[i]);
+        for(int i = 0 ; i < A.length; i++){
+        max_ending_here = max_ending_here + A[i];
+            if(max_so_far < max_ending_here){
+                max_so_far = max_ending_here;
+                start = s;
+                end = i;
             }
-
-            return max;
+            if(max_ending_here < 0){
+                max_ending_here = 0;
+                s = i+1;
+            }
+        }
+        return max_so_far;
     }
 }
