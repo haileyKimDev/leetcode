497c293dd189e19be0a21d143eb0ee58aee84051
haileykim2014 <haileykim2014@gmail.com>
2022-11-07 22:02:09 +0900

    Count bits

--- CountBit.java
+++ CountBit.java
@@ -1,9 +1,9 @@
 public class CountBit {
     public int[] countBits(int n) {
-        int res[] = new int[n + 1];
+        int res[] = new int[n + 1];//create an array
 
         for (int i = 0; i <= n; i++) {
-            res[i] = solve(i);
+            res[i] = solve(i); // enter the number of 1's binary representation of i
         }
         return res;
     }
@@ -15,8 +15,8 @@ public class CountBit {
         if (n == 1)
             return 1;
 
-        if(n%2==0)
-            return solve(n/2)
-        else return 1 + solve(n/2);
+        if(n%2==0) // if n is even,
+            return solve(n/2); // find quotient again
+        else return 1 + solve(n/2); // plus 1 with quotient
     }
 }
