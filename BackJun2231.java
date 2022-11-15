9fc30992023f1671bb378af212d280a0db1de973
haileykim2014 <haileykim2014@gmail.com>
2022-11-15 21:39:17 +0900

    Boj 11660

--- BackJun2231.java
+++ BackJun2231.java
@@ -19,7 +19,7 @@ public class BackJun2231 {
         int ret = n;
         while (n != 0) { // 10으로 나누었을때 몫이 0 이면 모든 자릿수 검사완
             ret += n % 10; // 10으로 나누고 나머지 더(n%10 :n의 1의 자리)
-            n /= 10; //
+            n /= 10; // (n의 10의자리 숫자)
         }
         return ret;
     }
