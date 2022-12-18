8b3db7068e6c5e44d30d7afdd650e5dadc77e409
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2022-12-19 00:22:27 +0900

    Time: 7 ms (14.84%), Space: 48.4 MB (5.10%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -12,12 +12,12 @@
 public class Solution {
         public boolean hasCycle(ListNode head) {
             Set<ListNode> set = new HashSet();
-            while (head != null) {
-                if (!set.add(head)) {
+            while (head != null) { //while head is not null
+                if (!set.add(head)) { //if set doens't contain head, that means set.add() returns true                                          //if set didn't contain head element
                     return true;
                 }
-                head = head.next;
+                head = head.next; //link next 
             }
-            return false;
+            return false; // when head is null
         }
     }
