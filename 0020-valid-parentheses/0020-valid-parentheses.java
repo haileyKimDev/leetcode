43e4a83d84d67c5d295901ed27e8fe70c45d5bd5
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2022-12-21 21:38:39 +0900

    Time: 1 ms (99.71%), Space: 40 MB (98.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,37 +2,17 @@
 // Solution
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            char paren = s.charAt(i);
-            
-            if (paren == '{' || paren == '[' || paren == '(') {
-                stack.offerFirst(paren);
-            }
-            else {
-                if (paren == '}') {
-                    if (stack.isEmpty() || stack.pollFirst() != '{') {
-                        return false;
-                    }
-                }
-                else if (paren == ']') {
-                    if (stack.isEmpty() || stack.pollFirst() != '[') {
-                        return false;
-                    }
-                }
-                else {
-                    if (stack.isEmpty() || stack.pollFirst() != '(') {
-                        return false;
-                    }
-                }
-            }
-        }
-        
-        if (!stack.isEmpty()) {
-            return false;
-        }
-        
-        return true;
+	Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
     }
 }
