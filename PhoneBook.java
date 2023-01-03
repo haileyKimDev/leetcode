9e321a877b8880252a438c8cd35bd0d7a67c4804
haileykim2014 <haileykim2014@gmail.com>
2023-01-04 01:57:57 +0900

    phone book

--- PhoneBook.java
+++ PhoneBook.java
@@ -3,11 +3,11 @@ import java.util.HashMap;
 public class PhoneBook {
     public boolean solution(String[] phone_book){
         HashMap<String,Integer> map = new HashMap<>();
-
+        //1. create HashMap
         for(int i = 0 ; i < phone_book.length;i++){
             map.put(phone_book[i],1);
         }
-
+        //2.check if the hashMap contains prefix of every phone numbers
         for(int i= 0 ; i < phone_book.length;i++){
             for(int j = 1 ;j < phone_book[i].length(); j++){
                 if(map.containsKey(phone_book[i].substring(0,j)))
