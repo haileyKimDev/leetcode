c5be3c6fb73d04384ef310acadfe378631394873
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2023-12-27 23:21:59 +0900

    Time: 1 ms (99.99%), Space: 57 MB (90.08%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,25 +1,20 @@
 class Solution {
-    public int maxSubArray(int[] A) {
-        int max_so_far = A[0]; //maximun sum
-        int max_ending_here = 0; // 
-        int start = 0; // first element of Maximum Subarray
-        int end = 0;// last element of Maximum Subarray
-        int s=0; // which indicate where Maximum Subarray can start
-
-        for(int i = 0 ; i < A.length; i++){
-        max_ending_here = max_ending_here + A[i];
-            if(max_so_far < max_ending_here){ 
-                // if current sum is greater than max so far
-                max_so_far = max_ending_here;
-                start = s;
-                end = i;
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
             }
-            if(max_ending_here < 0){
-                // current sub array is no longer exists because the sum is negative
-                max_ending_here = 0;
-                s = i+1; // to find a new sub arrary 
+            
+            if (currentSum < 0) {
+                currentSum = 0;
             }
         }
-        return max_so_far;
+        
+        return maxSum;
     }
 }
