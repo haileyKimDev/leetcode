765cbdb4aaba5effc8f90f642d7597d92b789a3c
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-02-06 22:49:47 +0900

    Time: 2 ms (94.86%), Space: 44.8 MB (53.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,24 +1,16 @@
 class Solution {
-        // time complexity : for double loop -> O(n^2)
-        // space complexity : no extra space allocated -> O(1)
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
 
-        for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i])){
-                return new int[]{map.get(nums[i]), i};
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
             }
-            map.put(target - nums[i], i);
+            numMap.put(nums[i], i);
         }
 
-        return new int[]{};
+        return new int[]{}; // No solution found
     }
-    
-        public static void main(String[] args) {
-            int[] nums = {1, 4, 7, 8};
-            int target = 11;
-
-            Solution t = new Solution();
-            System.out.println(Arrays.toString(t.twoSum(nums, target)));
-        }
 }
