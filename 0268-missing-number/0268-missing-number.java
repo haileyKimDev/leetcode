cb151dd108031a5b747e3422382eb6e9289c80b0
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-02-18 03:46:20 +0900

    Time: 0 ms (100%), Space: 45 MB (83.39%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,25 +1,9 @@
-class Solution {
+public class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-
-        // Ensure that n is at the last index
-        if (nums[nums.length-1] != nums.length) {
-            return nums.length;
-        }
-        // Ensure that 0 is at the first index
-        else if (nums[0] != 0) {
-            return 0;
-        }
-
-        // If we get here, then the missing number is on the range (0, n)
-        for (int i = 1; i < nums.length; i++) {
-            int expectedNum = nums[i-1] + 1;
-            if (nums[i] != expectedNum) {
-                return expectedNum;
-            }
-        }
-
-        // Array was not missing any numbers
-        return -1;
+        int sum = 0;
+        for(int num: nums)
+            sum += num;
+            
+        return (nums.length * (nums.length + 1) )/ 2 - sum;
     }
 }
