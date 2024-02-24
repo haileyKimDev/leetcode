4bb945d0e58688cf98512a36d513430f7a9f36ea
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-02-25 00:23:57 +0900

    Time: 0 ms (100%), Space: 42.3 MB (71.63%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,35 +1,25 @@
 /**
- * https://leetcode.com/problems/reverse-linked-list/
- *
- * O(n) runtime, O(1) space
- *
- * this question can be solved by using an iteration method or a recursion method
- */
-
-/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
-// iteration
-public class Solution {
-public ListNode reverseList(ListNode node)
-    {
-        ListNode prev = null;
-        ListNode current = node;
-        ListNode next = null;
-        while (current != null) {
-            next = current.next;
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;  
+        ListNode current = head;
+    
+        
+        while(current != null) { 
+            ListNode next = current.next; 
             current.next = prev;
             prev = current;
             current = next;
         }
-        node = prev;
-        return node;
+       return prev; 
     }
 }
-
