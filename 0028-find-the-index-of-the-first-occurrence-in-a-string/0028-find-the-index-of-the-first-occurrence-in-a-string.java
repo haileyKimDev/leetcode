680d16bacedf778963a28066cb7a317f1fc9f24f
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-04-19 20:06:14 +0900

    Time: 0 ms (100%), Space: 41.3 MB (69.58%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,9 @@
+class Solution {
+  public static int strStr(String haystack, String needle) {
+    if(haystack.contains(needle)){
+      return haystack.indexOf(needle);
+    } else {
+      return -1;
+    }
+  }
+}
