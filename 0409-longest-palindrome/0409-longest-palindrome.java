7380057020fa9b316c938608353d79a1703078f9
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-05-13 22:15:34 +0900

    Time: 9 ms (24.33%), Space: 41.8 MB (37.98%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,17 +1,16 @@
 class Solution {
     public int longestPalindrome(String s) {
-            if(s==null || s.length()==0) return 0;
-            HashSet<Character> hs = new HashSet<Character>();
-            int count = 0;
-            for(int i=0; i<s.length(); i++){
-                if(hs.contains(s.charAt(i))){
-                    hs.remove(s.charAt(i));
-                    count++;
-                }else{
-                    hs.add(s.charAt(i));
-                }
-            }
-            if(!hs.isEmpty()) return count*2+1;
-            return count*2;
+        int oddCount = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            if (map.get(ch) % 2 == 1)
+                oddCount++;
+            else
+                oddCount--;
+        }
+        if (oddCount > 1)
+            return s.length() - oddCount + 1;
+        return s.length();
     }
 }
