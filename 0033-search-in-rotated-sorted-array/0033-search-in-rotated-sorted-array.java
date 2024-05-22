76efac6f6df2d346f212590d5040c3ad6beeca41
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-05-22 21:51:11 +0900

    Time: 0 ms (100%), Space: 41.8 MB (73.92%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,33 @@
+class Solution {
+  public static int search(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+
+      if (nums[mid] == target) {
+        return mid;
+      }
+
+      // Left side is sorted
+      if (nums[left] <= nums[mid]) {
+        if (nums[left] <= target && target < nums[mid]) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      }
+      // Right side is sorted
+      else {
+        if (nums[mid] < target && target <= nums[right]) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+    }
+
+    return -1; // target이 배열에 존재하지 않는 경우
+  }
+}
