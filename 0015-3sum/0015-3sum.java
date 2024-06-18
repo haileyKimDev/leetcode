375b07ddff2194eb4b8750f3b8e1f5cb06c0497c
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-06-18 20:43:52 +0900

    Time: 833 ms (18.5%), Space: 51.9 MB (43.84%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,48 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Sort the array
+        int target = 0;
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
             int j = i + 1;
             int k = nums.length - 1;
-
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found a triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    // Move the pointers
+                if (sum == target) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
-                } else if (sum < 0) {
-                    // Sum is less than zero, increment j to increase the sum
+                } else if (sum < target) {
                     j++;
                 } else {
-                    // Sum is greater than zero, decrement k to decrease the sum
                     k--;
                 }
             }
         }
-        return ans;
+        output.addAll(s);
+        return output;
     }
 }
