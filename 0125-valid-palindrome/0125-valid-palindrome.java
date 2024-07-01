205a8f2806e20e82cb18a4166bc1412db960f9c0
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-07-01 20:10:47 +0900

    Time: 13 ms (40.28%), Space: 45.2 MB (16.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,18 @@
 class Solution {
-    public static boolean isPalindrome(String s) {
-        // 정규 표현식을 사용하여 비알파벳 문자 제거 및 소문자로 변환
-        String filtered = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        // 양 끝에서 시작하여 중앙으로 이동하면서 문자 비교
-        int left = 0, right = filtered.length() - 1;
-        while (left < right) {
-            if (filtered.charAt(left) != filtered.charAt(right)) {
-                return false; // 양 끝의 문자가 다르면 회문이 아님
+    public boolean isPalindrome(String s) {
+       String noSpaces = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int start = 0 ;
+        int end = noSpaces.length()-1;
+        while(start <= end){
+            char startChar = noSpaces.charAt(start);
+            char endChar = noSpaces.charAt(end);
+            if(startChar != endChar){
+                return false;
             }
-            left++;  // 왼쪽 인덱스를 오른쪽으로 이동
-            right--; // 오른쪽 인덱스를 왼쪽으로 이동
+            start++;
+            end--;
         }
-        return true; // 모든 검사를 통과하면 회문임
-    }
 
+        return true;
+    }
 }
