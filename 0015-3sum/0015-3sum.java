7fdedd7a2b195dab3346744ddd872f17aea0f896
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-07-03 22:02:05 +0900

    Time: 526 ms (23.58%), Space: 52.8 MB (13.67%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,48 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Sort the array
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found a triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    // Move the pointers
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    // Sum is less than zero, increment j to increase the sum
-                    j++;
-                } else {
-                    // Sum is greater than zero, decrement k to decrease the sum
-                    k--;
+        Set<List<Integer>> ans=new HashSet<>();
+        for(int i = 0; i < nums.length-2; i++){
+            int p1 = i+1;
+            int p2 = nums.length-1;
+            while(p1 < p2){
+                int sum = nums[i]+nums[p1]+nums[p2];
+                if(sum == 0){
+                    ArrayList<Integer> sp = new ArrayList<>();
+                    sp.add(nums[i]);
+                    sp.add(nums[p1]);
+                    sp.add(nums[p2]);
+                    
+                    ans.add(sp);
+                    p1++;
+                }
+                else if(sum < 0){
+                    p1++;
+                }
+                else{
+                    p2--;
                 }
             }
         }
-        return ans;
+        return new ArrayList<>(ans);
     }
 }
