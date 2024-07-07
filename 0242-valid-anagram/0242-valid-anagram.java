8bce38c29550cf86a45f3910ee33fc9bafa326b9
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-07-07 16:13:10 +0900

    Time: 3 ms (88.68%), Space: 44.7 MB (20.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> count = new HashMap<>();
-        
-        // Count the frequency of characters in string s
-        for (char x : s.toCharArray()) {
-            count.put(x, count.getOrDefault(x, 0) + 1);
+        if(s.length() != t.length()){
+            return false;
         }
-        
-        // Decrement the frequency of characters in string t
-        for (char x : t.toCharArray()) {
-            count.put(x, count.getOrDefault(x, 0) - 1);
-        }
-        
-        // Check if any character has non-zero frequency
-        for (int val : count.values()) {
-            if (val != 0) {
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        for(int i = 0 ; i < s.length();i++){
+            if(sArr[i]!=tArr[i]){
                 return false;
             }
         }
-        
         return true;
     }
 }
