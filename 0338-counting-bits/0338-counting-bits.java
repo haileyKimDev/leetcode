5704e98d7bd88105cdef9d9bf995a5a0254d300f
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-07-07 19:32:43 +0900

    Time: 2 ms (92.97%), Space: 46.7 MB (24.16%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,14 +1,15 @@
 public class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n + 1];
-        int offset = 1; // 현재 범위 내에서 가장 높은 2의 거듭제곱
-
-        for (int i = 1; i <= n; i++) {
-            if (i == offset * 2) {
-                offset = i; // 가장 높은 2의 거듭제곱을 갱신
+        int[] arr = new int[n+1];
+        int offset = 1;
+        for(int i = 1 ; i <= n ;i++){
+            if(i == offset*2){
+                offset = i;
             }
-            ans[i] = 1 + ans[i - offset];
+
+            arr[i] = 1 + arr[i-offset];
         }
-        return ans;
+
+        return arr;
     }
 }
