1cb53288663d1db9610233f472f2d92822e0fb54
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-07-08 00:56:38 +0900

    Time: 7 ms (41.99%), Space: 44.4 MB (57.99%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -6,14 +6,13 @@ class Solution {
         while (i < s.length()) {
             char c = s.charAt(i);
             while (set.contains(c)) {
-                set.remove(s.charAt(j));
-                j++;
+                set.remove(s.charAt(j)); // j 위치의 문자를 제거
+                j++; // 윈도우 시작점을 오른쪽으로 이동
             }
-            set.add(c);
-            maxLen = Math.max(maxLen, i - j + 1);
-            i++;
+            set.add(c); // 새로운 문자를 윈도우에 추가
+            maxLen = Math.max(maxLen, i - j + 1); // 최대 길이 갱신
+            i++; // 윈도우 끝점을 오른쪽으로 이동
         }
         return maxLen;
     }
-
 }
