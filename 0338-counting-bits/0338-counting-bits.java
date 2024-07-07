c1056778f8528f34943a3ce4038e5b57228306e9
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-07-07 18:27:48 +0900

    Time: 2 ms (92.97%), Space: 46.3 MB (84.76%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,22 +1,9 @@
-class Solution {
+public class Solution {
     public int[] countBits(int n) {
-       int res[] = new int[n+1]; //create int array
-
-       for(int i = 0 ; i<= n ; i++){
-           res[i] = solve(i); //enter the number of 1's in the binary representatio of 1
-       }
-       return res; // return array
-    }
-
-    public int solve(int n){
-        if(n==0)
-        return 0;
-
-        if(n==1)
-        return 1;
-
-        if(n%2==0) // if n is even
-        return solve(n/2);
-        else return 1 + solve(n/2);
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
     }
 }
