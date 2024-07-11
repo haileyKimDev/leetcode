7a11e24559b32d01f78e6cf14e190ff72bd98659
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-07-11 20:15:42 +0900

    Time: 5 ms (37.27%), Space: 44.7 MB (59.13%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,58 +1,37 @@
-
-
-Stack
-(3 / 7)
-
-
-Prerequisites
-
-Stacks
-Data Structures & Algorithms for Beginners
-Status
-Star
-Problem   
-Difficulty   
-Video Solution
-Code
-Valid Parentheses   	
-Min Stack   	
-Evaluate Reverse Polish Notation   	
-Generate Parentheses   	
-Daily Temperatures   	
-Car Fleet   	
-Largest Rectangle In Histogram   	
-View on Github
 class MinStack {
-
-    private Stack<Integer> stack;
-    private Stack<Integer> minStack;
+    Stack<Integer> stack ;
+    Stack<Integer> minStack;
 
     public MinStack() {
         stack = new Stack<>();
         minStack = new Stack<>();
     }
-
+    
     public void push(int val) {
         stack.push(val);
-
-        // The min stack may be empty, so we need to check it
-        val = Math.min(val, minStack.isEmpty() ? val : minStack.peek());
-        minStack.push(val);
+        if(minStack.isEmpty()){
+            minStack.push(val);
+        }else{
+            val = Math.min(val, minStack.peek());
+            minStack.push(val);
+        }
     }
-
+    
     public void pop() {
         stack.pop();
         minStack.pop();
     }
-
+    
     public int top() {
-        return stack.peek();
+       return stack.peek();
+    }
+    
+    public int getMin() {
+       return minStack.peek();
     }
 
-    public int getMin() {
-        return minStack.peek();
-    }
 }
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
@@ -61,8 +40,3 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
-Select Roadmap
-
-(19 / 150)
-
-
