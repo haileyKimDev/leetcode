2d2fbe871826ab118bf10ed9019cfdf0a92144ab
YOUNGHWA KIM <74589038+haileykim2014@users.noreply.github.com>
2024-07-30 14:03:27 +0900

    Time: 2 ms, Memory: 43.1 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,17 +1,12 @@
 class Solution {
-   public static boolean checkIfExist(int[] arr) {
-        HashSet<Integer> set = new HashSet<>();
-        
-        for (int num : arr) {
-            // Check if double or half of the number is already in the set
-            if (set.contains(2 * num) || (num % 2 == 0 && set.contains(num / 2))) {
-                return true;
+    public boolean checkIfExist(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && arr[i] == 2 * arr[j]) {
+                    return true;
+                }
             }
-            // Add the number to the set
-            set.add(num);
         }
-        
         return false;
     }
-
 }
