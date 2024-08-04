225fca8a4a2e6070b3cc5f57e2c91ecf76478cd9
YOUNGHWA KIM <74589038+haileyKimDev@users.noreply.github.com>
2024-08-05 06:11:59 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,13 @@
 class Solution {
-    //Time : O(n)
-    //Space : O(1)
-    public static void moveZeroes(int[] nums) {
-        int nonZeroIndex = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[nonZeroIndex++] = nums[i];
+    public void moveZeroes(int[] nums) {
+        int nonZero = 0 ; 
+        for(int i = 0 ; i < nums.length; i++){
+            if(nums[i]!=0){
+                nums[nonZero++] = nums[i];
             }
         }
-
-        for (int i = nonZeroIndex; i < nums.length; i++) {
-            nums[i] = 0;
+        for(int i = 1 ; i < nonZero; i++){
+            nums[nums.length-i] = 0;
         }
     }
 }
