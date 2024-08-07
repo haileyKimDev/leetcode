99eb7a91aad60eb020f36e3889941ae373e6dcfb
YOUNGHWA KIM <74589038+haileyKimDev@users.noreply.github.com>
2024-08-08 00:57:16 +0900

    Time: 1 ms (58.01%), Space: 44.8 MB (40.83%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,21 +1,16 @@
 class Solution {
     public int[] sortArrayByParity(int[] A) {
-        Integer[] B = new Integer[A.length];
-        for (int t = 0; t < A.length; ++t)
-            B[t] = A[t];
+        int[] ans = new int[A.length];
+        int t = 0;
 
-        Arrays.sort(B, (a, b) -> Integer.compare(a%2, b%2));
+        for (int i = 0; i < A.length; ++i)
+            if (A[i] % 2 == 0)
+                ans[t++] = A[i];
 
-        for (int t = 0; t < A.length; ++t)
-            A[t] = B[t];
-        return A;
+        for (int i = 0; i < A.length; ++i)
+            if (A[i] % 2 == 1)
+                ans[t++] = A[i];
 
-        /* Alternative:
-        return Arrays.stream(A)
-                     .boxed()
-                     .sorted((a, b) -> Integer.compare(a%2, b%2))
-                     .mapToInt(i -> i)
-                     .toArray();
-        */
+        return ans;
     }
 }
