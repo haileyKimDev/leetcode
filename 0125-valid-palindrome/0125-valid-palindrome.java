0d6a4e58e9fb1915cf0e8f30b78eb829637a965f
YOUNGHWA KIM <74589038+haileyKimDev@users.noreply.github.com>
2024-08-24 09:00:46 +0900

    Time: 5 ms (46.8%), Space: 44.4 MB (45.18%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       String noSpaces = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int start = 0 ;
-        int end = noSpaces.length()-1;
-        while(start <= end){
-            char startChar = noSpaces.charAt(start);
-            char endChar = noSpaces.charAt(end);
-            if(startChar != endChar){
+        String cleanedString = removeNonAlphanumeric(s);
+        int start = 0;
+        int end = cleanedString.length() - 1;
+        
+        while (start <= end) {
+            char startChar = cleanedString.charAt(start);
+            char endChar = cleanedString.charAt(end);
+            if (startChar != endChar) {
                 return false;
             }
             start++;
             end--;
         }
-
+        
         return true;
+    }
+
+    public String removeNonAlphanumeric(String s) {
+        StringBuilder str = new StringBuilder();
+        char[] arr = s.toLowerCase().toCharArray();
+        for (char c : arr) {
+            if (Character.isLetterOrDigit(c)) {
+                str.append(c);
+            }
+        }
+        return str.toString();
     }
 }
