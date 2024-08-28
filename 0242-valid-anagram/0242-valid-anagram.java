4ecdbf0105991061f8c0c79eb0c5430a987aac68
YOUNGHWA KIM <74589038+haileyKimDev@users.noreply.github.com>
2024-08-28 18:33:52 +0900

    Time: 2 ms (97.26%), Space: 43 MB (80.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] count = new int[26];
+        int[] count = new int[26];  // Create an array to count the frequency of each letter (assuming only lowercase a-z)
         
         // Count the frequency of characters in string s
         for (char x : s.toCharArray()) {
-            count[x - 'a']++;
+            count[x - 'a']++;  // Increment the count for the corresponding character in s
         }
         
         // Decrement the frequency of characters in string t
         for (char x : t.toCharArray()) {
-            count[x - 'a']--;
+            count[x - 'a']--;  // Decrement the count for the corresponding character in t
         }
         
-        // Check if any character has non-zero frequency
+        // Check if any character has a non-zero frequency
         for (int val : count) {
-            if (val != 0) {
+            if (val != 0) {  // If any count is not zero, the strings are not anagrams
                 return false;
             }
         }
         
-        return true;
+        return true;  // If all counts are zero, the strings are anagrams
     }
 }
