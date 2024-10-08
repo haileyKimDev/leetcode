191f12c55f6696d88216f78d51a197c2b7542002
YOUNGHWA KIM <74589038+haileyKimDev@users.noreply.github.com>
2024-10-09 00:14:48 +0900

    Time: 1 ms (65.81%), Space: 42.1 MB (46.02%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return new ArrayList<Integer>();
+
+        ArrayDeque<TreeNode> nextLevel = new ArrayDeque() {
+            {
+                offer(root);
+            }
+        };
+        ArrayDeque<TreeNode> currLevel = new ArrayDeque();
+        List<Integer> rightside = new ArrayList();
+
+        TreeNode node = null;
+        while (!nextLevel.isEmpty()) {
+            // prepare for the next level
+            currLevel = nextLevel;
+            nextLevel = new ArrayDeque<>();
+
+            while (!currLevel.isEmpty()) {
+                node = currLevel.poll();
+
+                // add child nodes of the current level
+                // in the queue for the next level
+                if (node.left != null) nextLevel.offer(node.left);
+                if (node.right != null) nextLevel.offer(node.right);
+            }
+
+            // The current level is finished.
+            // Its last element is the rightmost one.
+            if (currLevel.isEmpty()) rightside.add(node.val);
+        }
+        return rightside;
+    }
+}
